e2f3d7ee9333e2885b9a9553274abc3ea1938225
Daniel Donjuan <ddonjuan57@gmail.com>
2020-02-28 00:48:00 -0800

    added context, node-server, and changed running scripts to run client and server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const server = express();
+
+
+
+//static folder
+server.use(express.static(path.join(__dirname + 'public')));
+
+
+//body parser
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended : false}));
+
+
+
+
+// require("./routes")(server);
+
+
+
+server.listen(3001, () => {
+    console.log("server is starting...server is listening on port 3001");
+});
