c680bc94d8e0b7ad158e061a5eed38ea7259b7f6
Daniel Donjuan <ddonjuan57@gmail.com>
2020-03-03 23:27:12 -0800

    When user clicks on login, nav links disappear and then re-appear when you click back on Test Fire logo

--- src/components/navigation_bars/landing-navbar.js
+++ src/components/navigation_bars/landing-navbar.js
@@ -6,24 +6,30 @@ class LandingNavbar extends Component {
     constructor(props) {
         super(props);
     }
+    displayLinks() {
+        return (
+            <div className="navbar-nav">
+                <a className="nav-item nav-link active" href="#about">Home</a>
+                <a className="nav-item nav-link" href="#">Features</a>
+                <a className="nav-item nav-link" href="#">Pricing</a>
+            </div>
+        )
+    }
 
     render() {
+        const toggleLinks = this.props.toggleLinks ? "" : this.displayLinks();
         return (
             <nav className="navbar navbar-expand-sm navbar-expand-md navbar-expand-lg navbar-dark bg-dark">
-                <Link id="nav-brand" className="navbar-brand" to="/">Test Fire</Link>
+                <Link onClick={this.props.toggleLinksOn} id="nav-brand" className="navbar-brand" to="/">Test Fire</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <a className="nav-item nav-link active" href="#about">Home</a>
-                        <a className="nav-item nav-link" href="#">Features</a>
-                        <a className="nav-item nav-link" href="#">Pricing</a>
-                    </div> 
+                    {toggleLinks}
                 </div>
 
                 <ul id="ul-links" className="navbar-nav">
-                    <Link to="/sign-in"><button className="btn btn-outline-light">Login</button></Link>
+                    <Link onClick={this.props.toggleLinksOff} to="/sign-in"><button  className="btn btn-outline-light">Login</button></Link>
                 </ul>
             </nav>
         )
