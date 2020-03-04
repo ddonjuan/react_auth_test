58a60d4453dafb4a4bf65556f02790b44d8007b5
Daniel Donjuan <ddonjuan57@gmail.com>
2020-03-03 22:16:37 -0800

    finished styling all auth pages

--- src/components/auth_pages/create-account.js
+++ src/components/auth_pages/create-account.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import InputField from '../helpers/inputs/inputs';
 import { MyContext } from '../config/firebase';
 import { withRouter } from 'react-router-dom';
+import "../../stylesheets/auth_pages/helpers.css";
+import "../../stylesheets/auth_pages/sign-in.css";
 
 class SignUp extends Component {
     constructor(props) {
@@ -15,20 +17,22 @@ class SignUp extends Component {
             <MyContext.Consumer>
                 {(context) => (
                     <React.Fragment>
-                        <form onSubmit={(e)=>{e.preventDefault();context.signUp()}}>
-                            <div className="row d-flex justify-content-center">
-                                <div className="logo">Create An Account</div>
-                            </div>
-                            <div className="row d-flex justify-content-center">
-                                <div id="sign-in" className="col col-sm-6 col-md-6 col-lg-3">
-                                    <InputField onChange={context.handleInputChange} value={context.email} labelName="Email" inputID="email" inputType="email" smallText="Please enter your email" />
-                                    <InputField onChange={context.handleInputChange} value={context.password} labelName="Choose your password" inputID="password" inputType="password" smallText="Please enter your chosen password" />
-                                    <InputField onChange={context.handleInputChange} value={context.confirmPassword} labelName="Confirm your password" inputID="confirmPassword" inputType="password" smallText="Please confirm your password" />
-                                    <button type="submit" className="btn btn-primary">Create Account</button>
+                        <form className="sign-in-container auth-pages-picture" onSubmit={(e) => { e.preventDefault(); context.signUp() }}>
+                            <div className="info-parent-container">
+
+                                <div className="row d-flex justify-content-center">
+                                    <div className="logo">Create An Account</div>
                                 </div>
+                                <div className="row d-flex justify-content-center">
+                                    <div id="sign-in" className="col-10 col-sm-4 col-md-8 col-lg-6">
+                                        <InputField onChange={context.handleInputChange} value={context.email} labelName="Email" inputID="email" inputType="email" smallText="Please enter your email" />
+                                        <InputField onChange={context.handleInputChange} value={context.password} labelName="Choose your password" inputID="password" inputType="password" smallText="Please enter your chosen password" />
+                                        <InputField onChange={context.handleInputChange} value={context.confirmPassword} labelName="Confirm your password" inputID="confirmPassword" inputType="password" smallText="Please confirm your password" />
+                                        <button type="submit" className="btn btn-primary">Create Account</button>
+                                    </div>
 
+                                </div>
                             </div>
-
                         </form>
                     </React.Fragment>
                 )}
--- src/components/auth_pages/sign-in.js
+++ src/components/auth_pages/sign-in.js
@@ -3,7 +3,7 @@ import InputField from '../helpers/inputs/inputs';
 import { MyContext } from '../config/firebase';
 import { Link, withRouter } from 'react-router-dom';
 import "../../stylesheets/auth_pages/helpers.css";
-
+import "../../stylesheets/auth_pages/sign-in.css";
 class SignIn extends Component {
     constructor(props) {
         super(props);
@@ -16,35 +16,37 @@ class SignIn extends Component {
             <MyContext.Consumer>
                 {(context) => (
                     <React.Fragment>
-                        <form onSubmit={(e)=>{e.preventDefault();context.signIn()}}>
-
-                            <div className="row d-flex justify-content-center">
-                                <div className="logo">Company Name</div>
-                            </div>
-                            <div id="input-container" className="row d-flex justify-content-center">
-                                <div id="sign-in" className="col-10 col-sm-4 col-md-5 col-lg-3">
-                                    <InputField
-                                        onChange={context.handleInputChange}
-                                        labelName="Email"
-                                        value={context.state.email}
-                                        inputID="email"
-                                        inputType="email"
-                                        smallText="Please enter your email"
-                                    />
-                                    <InputField
-                                        onChange={context.handleInputChange}
-                                        labelName="Password"
-                                        value={context.state.password}
-                                        inputID="password"
-                                        inputType="password"
-                                        smallText="Please enter your password"
-                                    />
-                                    <div className="auth-btn-space"><Link to="/create-account">Sign Up</Link></div>
-                                    <div className="auth-btn-space"><Link to="/reset-password">Forgot Password</Link></div>
-                                    <button type="submit" className="btn btn-primary">Sign In</button>
+                        <form className="sign-in-container auth-pages-picture" onSubmit={(e) => { e.preventDefault(); context.signIn() }}>
+                            <div className="info-parent-container">
+                                <div className="row d-flex justify-content-center">
+                                    <h2 className="logo">Login </h2>
                                 </div>
+                                <div id="input-container" className="row d-flex justify-content-center">
+                                    <div id="sign-in" className="col-10 col-sm-4 col-md-8 col-lg-6">
+                                        <InputField
+                                            onChange={context.handleInputChange}
+                                            labelName="Email"
+                                            value={context.state.email}
+                                            inputID="email"
+                                            inputType="email"
+                                            smallText="Please enter your email"
+                                        />
+                                        <InputField
+                                            onChange={context.handleInputChange}
+                                            labelName="Password"
+                                            value={context.state.password}
+                                            inputID="password"
+                                            inputType="password"
+                                            smallText="Please enter your password"
+                                        />
+                                        <div className="auth-btn-space"><Link to="/create-account">Sign Up</Link></div>
+                                        <div className="auth-btn-space"><Link to="/reset-password">Forgot Password</Link></div>
+                                        <button type="submit" className="btn btn-primary">Sign In</button>
+                                    </div>
 
+                                </div>
                             </div>
+
                         </form>
 
                     </React.Fragment>
